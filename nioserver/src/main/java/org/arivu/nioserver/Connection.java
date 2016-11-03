899090c0a7b50a1dc0c8557d1cab21d5ca9838e6
parthipan.gounder@ril.com <parthipangounder@192.168.5.101>
2016-11-03 20:51:31 +0530

    Removed close method on ByteData.

--- nioserver/src/main/java/org/arivu/nioserver/Connection.java
+++ nioserver/src/main/java/org/arivu/nioserver/Connection.java
@@ -321,7 +321,7 @@ final class Connection {
 
 }
 final class ConnectionState{
-	private static final Logger logger = LoggerFactory.getLogger(ConnectionState.class);
+//	private static final Logger logger = LoggerFactory.getLogger(ConnectionState.class);
 	// Write state
 	int writeLen = 0;
 	ByteData poll = null;
@@ -353,13 +353,13 @@ final class ConnectionState{
 	}
 
 	void clearBytes() {
-		if(poll!=null){
-			try {
-				poll.close();
-			} catch (IOException e) {
-				logger.error("Error closing RandomAccessFile :: ", e);
-			}
-		}
+//		if(poll!=null){
+//			try {
+//				poll.close();
+//			} catch (IOException e) {
+//				logger.error("Error closing RandomAccessFile :: ", e);
+//			}
+//		}
 		poll = null;
 		pos = 0;
 		rem = 0;	
